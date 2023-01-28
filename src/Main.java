c3ff42fe0f354c0870fedd553af38be773a09db2
minjo-on <alswns11346@gmail.com>
2023-01-28 21:06:03 +0900

    Baekjoon.10807

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,16 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        int count = 1;
-        int max = 0;
-        int a = 1;
         Scanner sc = new Scanner(System.in);
-        int n[] = new int[3];
-        for(int i=0;i<3;i++){
+        int total = sc.nextInt();
+        int totalNum = sc.nextInt();
+        int sum = 0;
+        for(int i=0;i<totalNum;i++){
+            int price = sc.nextInt();
             int num = sc.nextInt();
-            n[i] = num;
-            if(num>max) max=num;
+            sum += price * num;
         }
-
-        if(n[0]==n[1]&&n[1]==n[2]) {
-            count+=2;
-        }else if(n[0]==n[1] || n[1]==n[2]){
-            count++;
-            a = n[1];
-        }else if(n[0]==n[2]){
-            count++;
-            a = n[0];
-        }
-
-        switch(count){
-            case 3: System.out.println(10000+max*1000); break;
-            case 2: System.out.println(1000+a*100); break;
-            case 1: System.out.println(max*100); break;
-        }
+        if(total == sum) System.out.println("Yes");
+        else System.out.println("No");
     }
 }
