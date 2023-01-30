257818d9e0c598ecae0f3536268fec6dedc53b42
minjo-on <alswns11346@gmail.com>
2023-01-30 21:11:35 +0900

    Baekjoon.1978

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Main{
     static boolean checkNum(int n){
         if(n==1) return false;
-        for(int i=2;i<Math.sqrt(n);i++){
+        for(int i=2;i<=Math.sqrt(n);i++){
             if(n%i==0) return false;
         }
         return true;
@@ -11,9 +11,12 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); //입력할 정수 개수
         int count = 0; //소수 개수
-        for(int i=0;i<n;i++){
-            int t = sc.nextInt();
-            if(checkNum(t)) count++;
+        for(int i=1;i<=999;i++){
+            //int t = sc.nextInt();
+            if(checkNum(i)) {
+                System.out.println(i);
+                count++;
+            }
         }
         System.out.println(count);
     }
