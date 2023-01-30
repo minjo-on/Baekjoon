9f352b65c01c090c9341eca00a6c14f9640af094
minjo-on <alswns11346@gmail.com>
2023-01-30 22:11:40 +0900

    Baekjoon.1929

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
 import java.util.*;
 public class Main{
-    /*static boolean checkNum(int n){
+    static boolean checkNum(int n){
         if(n==1) return false;
         for(int i=2;i<=Math.sqrt(n);i++){
             if(n%i==0) return false;
         }
         return true;
-    }*/
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
         int n = sc.nextInt();
-        for(int i=2;i<=Math.sqrt(n);i++) {
-            while (n % i == 0) {
-                System.out.println(i);
-                n /= i;
-            }
+
+        for(int i = m;i<=n;i++){
+            if(checkNum(i)) System.out.println(i);
         }
-        if(n!=1) System.out.println(n);
     }
 }
