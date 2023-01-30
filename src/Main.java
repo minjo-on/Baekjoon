c2aee34627e59071d7ec9f272b42da60c9d5b68b
minjo-on <alswns11346@gmail.com>
2023-01-30 22:01:45 +0900

    Baekjoon.11653

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,6 @@ public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int count = 2;//인수분해할 수
         for(int i=2;i<=Math.sqrt(n);i++) {
             while (n % i == 0) {
                 System.out.println(i);
