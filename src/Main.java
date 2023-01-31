8b9ac708e11dd150e0edd2e2fc370ac056063048
minjo-on <alswns11346@gmail.com>
2023-01-31 23:28:09 +0900

    Baekjoon.9020

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,26 @@ public class Main{
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         var array = new ArrayList<Integer>();
-        while(true){
-            int n = sc.nextInt();
-            if(n==0)break;
-            array.add(n);
+        for(int i=0;i<n;i++){
+            int c = sc.nextInt();
+            array.add(c);
         }
 
         for(int i=0;i<array.size();i++){
             int num = array.get(i);
-            int count = 0;
-            for(int k=num+1;k<=2*num;k++){
-                if(checkNum(k)) count++;
+            int a = num/2;
+            int b = num/2;
+            while(true){
+                if(checkNum(a)&&checkNum(b)) break;
+                else{
+                    a--;
+                    b++;
+                }
             }
-            System.out.println(count);
+            System.out.print(a+" ");
+            System.out.println(b);
         }
     }
 }
