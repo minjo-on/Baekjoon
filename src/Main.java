66d371a679ffcb580210875d16857179c5de531f
minjo-on <alswns11346@gmail.com>
2023-01-31 23:02:53 +0900

    Baekjoon.4948

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,20 @@ public class Main{
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
+        var array = new ArrayList<Integer>();
+        while(true){
+            int n = sc.nextInt();
+            if(n==0)break;
+            array.add(n);
+        }
 
-        for(int i = m;i<=n;i++){
-            if(checkNum(i)) System.out.println(i);
+        for(int i=0;i<array.size();i++){
+            int num = array.get(i);
+            int count = 0;
+            for(int k=num+1;k<=2*num;k++){
+                if(checkNum(k)) count++;
+            }
+            System.out.println(count);
         }
     }
 }
