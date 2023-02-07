cb82668649c787f7f90f3874eb5c611fc08b3d63
minjo-on <alswns11346@gmail.com>
2023-02-07 22:34:41 +0900

    Baekjoon.2751

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 import java.util.*;
 public class Main{
     public static void main(String[] args) {
+        StringBuffer s = new StringBuffer();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int p = sc.nextInt();
         var array = new ArrayList<Integer>();
         for(int i=0;i<n;i++){
             int num = sc.nextInt();
             array.add(num);
         }
         Collections.sort(array);
-        System.out.println(array.get(n-p));
+        for(int i=0;i<n;i++){
+            s.append(array.get(i)).append('\n');
+        }
+        System.out.println(s);
     }
 }
