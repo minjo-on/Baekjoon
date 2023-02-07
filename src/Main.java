61e75da06afb6f3a4f7e4e637cdab006f23034bb
minjo-on <alswns11346@gmail.com>
2023-02-07 22:21:51 +0900

    Baekjoon.25305

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,14 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int sum=0;
+        int n = sc.nextInt();
+        int p = sc.nextInt();
         var array = new ArrayList<Integer>();
-        for(int i=0;i<5;i++){
+        for(int i=0;i<n;i++){
             int num = sc.nextInt();
-            sum += num;
             array.add(num);
         }
         Collections.sort(array);
-        System.out.println(sum/5);
-        System.out.println(array.get(2));
+        System.out.println(array.get(n-p));
     }
 }
