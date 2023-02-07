f98c15edcc421c3385230efd894a4c7424712a1e
minjo-on <alswns11346@gmail.com>
2023-02-07 22:53:31 +0900

    Baekjoon.10989

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 import java.util.*;
-public class Main{
-    public static void main(String[] args) {
+import java.io.*;
+public class Main{//시간초과 나며 ㄴ뒤 진다 어? 됐다 ㅅ발 제발 제발 됐다ㅏㅏ아아앙아
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer s = new StringBuffer();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        var array = new ArrayList<Integer>();
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
         for(int i=0;i<n;i++){
-            int num = sc.nextInt();
-            array.add(num);
+            array[i] = Integer.parseInt(br.readLine());
         }
-        Collections.sort(array);
+        Arrays.sort(array);
         for(int i=0;i<n;i++){
-            s.append(array.get(i)).append('\n');
+            s.append(array[i]).append('\n');
         }
         System.out.println(s);
     }
