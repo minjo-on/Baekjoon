690e8da51bb3c0c6cc520f9c06b8a7fa037f1c3d
minjo-on <alswns11346@gmail.com>
2023-02-11 15:36:13 +0900

    Baekjoon.10870

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.util.*;
 public class Main{
-    static int factorial(int n){
-        if(n<=1) return 1;
-        else return n* factorial(n-1);
+    static int fibonacci(int n){
+        if(n==0) return 0;
+        else if (n==1) return 1;
+        else return fibonacci(n-1)+fibonacci(n-2);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int sum = factorial(n);
+        int sum = fibonacci(n);
         System.out.print(sum);
     }
 }
