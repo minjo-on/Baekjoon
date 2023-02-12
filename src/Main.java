83aa5b66d7471a91df5455b771fb71cba5afff65
minjo-on <alswns11346@gmail.com>
2023-02-12 17:27:46 +0900

    Baekjoon.11729

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 import java.util.*;
 public class Main{
-    static int fibonacci(int n){
-        if(n==0) return 0;
-        else if (n==1) return 1;
-        else return fibonacci(n-1)+fibonacci(n-2);
+    static StringBuilder sb = new StringBuilder();
+    static void hanoiTower(int n, int start, int mid, int last){
+        if(n==1){
+            sb.append(start+" "+last+"\n");
+            return;
+        }
+        hanoiTower(n-1,start,last,mid);
+        sb.append(start+" "+last+"\n");
+        hanoiTower(n-1,mid,start,last);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int sum = fibonacci(n);
-        System.out.print(sum);
+        sb.append((int)(Math.pow(2,n)-1)+"\n");
+        hanoiTower(n,1,2,3);
+        System.out.print(sb);
     }
 }
