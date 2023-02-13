30e3821bc71bc5b2813b3a9ed6bc657dec3e98e2
minjo-on <alswns11346@gmail.com>
2023-02-14 00:15:30 +0900

    Baekjoon.2231

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,26 @@
 import java.util.*;
-public class Main{
+public class Main{//뭐라도 해서 검색 조건 만들려다가 실패하고 그냥 0부터 쉣
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println(search(arr,n,k));
+        System.out.println(search(n));
     }
 
-    static int search(int[]arr, int n, int k){
-        int result = 0;
-        for(int i=0;i<n-2;i++){
-
-            if(arr[i]>=k) continue;//첫번째 카드가 더 크면 패스
-            for(int j=i+1;j<n-1;j++){
-                if(arr[i]+arr[j]>=k) continue;//두 카드의 합이 더 크면 패스
-                for(int l=j+1;l<n;l++){
-                    int sum = arr[i]+arr[j]+arr[l];
-                    if(sum==k) return sum;//블랙잭 당첨
-                    if(sum>result&&sum<k) result = sum;//낼 수 있는 최대 값
-                }
+    static int search(int n){
+        int test = 0;
+        while(test<=n){
+            int j = test;
+            int sum = 0;
+            while(j!=0){
+                sum+=j%10;
+                j/=10;
+            }
+            if(test+sum==n){
+                return test;
+            }else{
+                test++;
             }
         }
-        return result;
+        return 0;
     }
 }
