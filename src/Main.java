d0590bbb0a5c29718ba596be8203f0b08bba1d15
minjo-on <alswns11346@gmail.com>
2023-02-18 17:02:49 +0900

    Baekjoon.1436

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,74 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int row = sc.nextInt();//행
-        int col = sc.nextInt();//열
-        sc.nextLine();
-
-        int min = 64;//출력값
-
-        boolean[][] board = new boolean[row][col];//보드
-
-        //보드 입력
-        for(int i=0;i<row;i++){
-            String s = sc.nextLine();
-            for(int j=0;j<col;j++){
-                if(s.charAt(j)=='W') board[i][j]=true;
-                else board[i][j] = false;
-            }
-        }
-
-        //8*8로 나눠서 분석
-        for(int i=0;i<=row-8;i++){
-            for(int j=0;j<=col-8;j++){
-                int count = 0;//바꿔야 할 개수
-                boolean b = board[i][j];//첫번째 타입 색
-                for(int k=i;k<i+8;k++){
-                    for(int l=j;l<j+8;l++){
-                        if(board[k][l]!=b) count++;//만약 있어야 할 색과 다르면 카운트 증가
-                        b = !b;// 다음 있어야 할 색깔로 변경
+        int n = sc.nextInt();
+        int num = 0;
+        int first = 0;
+        int count = 1;
+        if(n>1){
+            /*case 5가지임
+            1. xxxx666xx
+            2. xxxx666
+            3. xxxx66
+            4. xxxx6
+            5. xxxx    */
+            while(true){
+                //first가 ~666~일때
+                if(first%10000/10==666&&first%10!=6){
+                    for(int i=0;i<1000;i++){
+                        if(count==n){
+                            System.out.println(first*1000+num);
+                            return;
+                        }
+                        count++;
+                        num++;
                     }
-                    b=!b;//이거도 마찬가지
+                    first++;
+                } else if (first%1000==666) {//first가 ~666일때
+                    num=0;
+                    for(int i=0;i<1000;i++){
+                        if(count==n){
+                            System.out.println(first*1000+num);
+                            return;
+                        }
+                        count++;
+                        num++;
+                    }
+                    first++;
+                } else if (first%100==66) {//first가 ~66일때
+                    num=600;
+                    for(int i=0;i<100;i++){
+                        if(count==n){
+                            System.out.println(first*1000+num);
+                            return;
+                        }
+                        count++;
+                        num++;
+                    }
+                    first++;
+                } else if (first%10==6) {//~6일때
+                    num = 660;
+                    for(int i=0;i<10;i++){
+                        if(count==n){
+                            System.out.println(first*1000+num);
+                            return;
+                        }
+                        count++;
+                        num++;
+                    }
+                    first++;
+                }else{//6이 아에 없을 때
+                    num = 666;
+                    if(count==n){
+                        System.out.println(first*1000+num);
+                        return;
+                    }
+                    count++;
+                    first++;
                 }
-                count = Math.min(count,64-count);//하는 이유는 반대 색깔로 바꿨을 때도 비교하려고
-                min = Math.min(min,count);//기존 최솟값과 비교해서 더 작은 값으로
             }
+        }else{
+            System.out.println(666);
         }
-        System.out.println(min);
     }
 }
