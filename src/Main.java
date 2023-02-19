ca156d3547f0f8c33c36369daa4613da18d488af
minjo-on <alswns11346@gmail.com>
2023-02-19 15:23:19 +0900

    Baekjoon.14425

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,17 @@
 import java.util.*;
-public class Main {//시간초과 해결
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
-        boolean flag = false;
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<n;i++){
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
         int s = sc.nextInt();
-
-        for(int i=0;i<s;i++){
-            int search = sc.nextInt();
-            int count;
-            int left = 0;
-            int right = n-1;
-            while(left<=right){
-                int mid = (left+right)/2;
-                if(arr[(left+right)/2]<search){
-                    left=mid+1;
-                }else if(arr[(left+right)/2]>search){
-                    right = mid-1;
-                }else{
-                    flag = true;
-                    break;
-                }
-            }
-            if(flag) sb.append(1+" ");
-            else sb.append(0+" ");
-            flag = false;
+        int count = 0;
+        HashMap<String,Integer> list = new HashMap<>();
+        for(int i=0;i<n;i++){
+            list.put(sc.next(),1);
         }
-        System.out.print(sb);
+        for(int i=0;i<s;i++){
+            if(list.containsKey(sc.next())) count++;
+        }
+        System.out.println(count);
     }
 }
