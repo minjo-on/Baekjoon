8f4469367cf7430ccf29b7eda4a1b70e0546eb77
minjo-on <alswns11346@gmail.com>
2023-02-21 22:56:02 +0900

    Baekjoon.9086

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringBuilder result = new StringBuilder();
-        int n = sc.nextInt();
-        sc.nextLine();
-        for(int i=0;i<n;i++){
-            String s = sc.nextLine();
-            result.append(s.charAt(0)+""+s.charAt(s.length()-1)+"\n");
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
         }
-        System.out.println(result);
-
     }
 }
