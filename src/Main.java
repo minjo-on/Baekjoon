265f43887d731b684edee951a318de9483a0e725
minjo-on <alswns11346@gmail.com>
2023-02-21 22:09:43 +0900

    Baekjoon.2743

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int arr[] = new int[n];
-
-        for(int i=0;i<n;i++){
-            arr[i] = i+1;
-        }
-        int temp;
-        for(int i=0;i<m;i++){
-            int j = sc.nextInt();
-            int k = sc.nextInt();
-            temp = arr[k-1];
-            arr[k-1] = arr[j-1];
-            arr[j-1] = temp;
-        }
-        for(int i=0;i<n;i++){
-            sb.append(arr[i]+" ");
-        }
-        System.out.println(sb);
+        String s = sc.nextLine();
+        System.out.println(s.length());
     }
 }
