fe0f9ab48880a3f38045e889cd31631e42a5bbb2
minjo-on <alswns11346@gmail.com>
2023-02-21 19:29:00 +0900

    Baekjoon.25314

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,15 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i] = i+1;
-        }
+        int[] arr = new int[n];
+
         int temp;
         for(int i=0;i<m;i++){
             int j = sc.nextInt();
             int k = sc.nextInt();
-            if(k-j==1){
-                temp = arr[k-1];
-                arr[k-1] = arr[j-1];
-                arr[j-1] = temp;
-            }else if(k==j) continue;
-            else{
-                int a = j-1;
-                int b = k-1;
-                while(true){
-                    temp = arr[a];
-                    arr[a] = arr[b];
-                    arr[b] = temp;
-                    a++;
-                    b--;
-                    if(a>=b) break;
-                }
+            int ball = sc.nextInt();
+            for(int l=j-1;l<k;l++){
+                arr[l] = ball;
             }
         }
         for(int i=0;i<n;i++){
