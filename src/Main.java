a88e3499627562bbe3ac13cfa61497f6bcf76664
minjo-on <alswns11346@gmail.com>
2023-02-21 19:20:34 +0900

    Baekjoon.25314

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,38 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
         StringBuilder sb = new StringBuilder();
-        while(n>0){
-            sb.append("long ");
-            n-=4;
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = i+1;
         }
-        sb.append("int ");
+        int temp;
+        for(int i=0;i<m;i++){
+            int j = sc.nextInt();
+            int k = sc.nextInt();
+            if(k-j==1){
+                temp = arr[k-1];
+                arr[k-1] = arr[j-1];
+                arr[j-1] = temp;
+            }else if(k==j) continue;
+            else{
+                int a = j-1;
+                int b = k-1;
+                while(true){
+                    temp = arr[a];
+                    arr[a] = arr[b];
+                    arr[b] = temp;
+                    a++;
+                    b--;
+                    if(a>=b) break;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            sb.append(arr[i]+" ");
+        }
         System.out.println(sb);
     }
 }
