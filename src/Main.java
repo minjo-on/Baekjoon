184e88bf607dbe5801bea135c57f34ae8b1345f0
minjo-on <alswns11346@gmail.com>
2023-02-22 20:21:04 +0900

    Baekjoon.5086

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[][]arr = new char[5][];
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<5;i++){
-            String s = sc.nextLine();
-            arr[i] = new char[s.length()];
-            for(int j=0;j<s.length();j++){
-                arr[i][j] = s.charAt(j);
-            }
+        while(true){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a==0&&b==0) break;
+            if(b%a==0) System.out.println("factor");
+            else if(a%b==0) System.out.println("multiple");
+            else System.out.println("neither");
         }
-        int length = 0;
-        for(int i=0;i<5;i++){
-            if(length<arr[i].length) length = arr[i].length;
-        }
-
-        for(int i=0;i<length;i++){
-            for(int j=0;j<5;j++){
-                if(arr[j].length<=i){
-                    continue;
-                }else sb.append(arr[j][i]);
-            }
-        }
-        System.out.println(sb);
     }
 }
