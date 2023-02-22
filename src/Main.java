c892107b9a075730c11bde2deb60c0b4c425bb16
minjo-on <alswns11346@gmail.com>
2023-02-22 20:28:37 +0900

    Baekjoon.2501

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(true){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            if(a==0&&b==0) break;
-            if(b%a==0) System.out.println("factor");
-            else if(a%b==0) System.out.println("multiple");
-            else System.out.println("neither");
+        int count = 0;
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                arr.add(i);
+                count++;
+            }
         }
+        if(s>count){
+            System.out.println(0);
+        }else System.out.println(arr.get(s-1));
     }
 }
