0e356230afae6ed0041740a45f95fce505c5c1ce
minjo-on <alswns11346@gmail.com>
2023-02-25 17:21:09 +0900

    Baekjoon.25206

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        int first = 0;
-        int last = s.length()-1;
-
-        while(first<=last){
-            if(s.charAt(first)==s.charAt(last)){
-                first++;
-                last--;
-            }else{
-                System.out.println(0);
-                return;
+        double credit = 0;
+        double sum = 0;
+        for(int i=0;i<20;i++) {
+            StringTokenizer st = new StringTokenizer(sc.nextLine(), " ");
+            st.nextToken();
+            double d = Double.parseDouble(st.nextToken());
+            String s = st.nextToken();
+            sum += d;
+            if (s.equals("A+")) {
+                credit += d * 4.5;
+            } else if (s.equals("A0")) {
+                credit += d * 4;
+            } else if (s.equals("B+")) {
+                credit += d * 3.5;
+            } else if (s.equals("B0")) {
+                credit += d * 3;
+            } else if (s.equals("C+")) {
+                credit += d * 2.5;
+            } else if (s.equals("C0")) {
+                credit += d * 2;
+            } else if (s.equals("D+")) {
+                credit += d * 1.5;
+            } else if (s.equals("D0")) {
+                credit += d;
+            } else if (s.equals("P")) {
+                sum -= d;
             }
         }
-        System.out.println(1);
+        System.out.printf("%.6f",credit/sum);
     }
 }
