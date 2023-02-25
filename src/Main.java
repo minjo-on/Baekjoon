d9a8240b251c5c53ac88b1c49eb463e6d7651a05
minjo-on <alswns11346@gmail.com>
2023-02-25 16:58:58 +0900

    Baekjoon.10812

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,20 @@
 import java.util.*;
 public class Main {
-    static int[] basket;
-    static void rocation(int i,int j,int k){
-        int temp = basket[k];
-        while(basket[i]!=temp){
-            for(int l=0;l<=j-k;l++){
-                int num = basket[j];
-                for(int m=j;m>i;m--){
-                    basket[m] = basket[m-1];
-                }
-                basket[i] = num;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int first = 0;
+        int last = s.length()-1;
+
+        while(first<=last){
+            if(s.charAt(first)==s.charAt(last)){
+                first++;
+                last--;
+            }else{
+                System.out.println(0);
+                return;
             }
         }
-    }
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        basket = new int[n];
-        for(int l=0;l<n;l++){
-            basket[l] = l+1;
-        }
-        for(int l=0;l<m;l++){
-            int i = sc.nextInt()-1;
-            int j = sc.nextInt()-1;
-            int k = sc.nextInt()-1;
-            if(i!=k) rocation(i,j,k);
-        }
-        for(int l=0;l<basket.length;l++){
-            sb.append(basket[l]+" ");
-        }
-        System.out.println(sb);
+        System.out.println(1);
     }
 }
