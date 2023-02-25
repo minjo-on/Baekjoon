e6cc9e7d8d4c9c639fa168745d23cb7f129c9c16
minjo-on <alswns11346@gmail.com>
2023-02-25 17:36:01 +0900

    Baekjoon.24263

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println('1');
-        System.out.println('0');
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n+"\n"+1);
     }
 }
