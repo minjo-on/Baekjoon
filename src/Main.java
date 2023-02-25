94543ddfb27e528004819c4b5569358df6cff3f4
minjo-on <alswns11346@gmail.com>
2023-02-25 18:00:09 +0900

    Baekjoon.24264

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(n+"\n"+1);
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        Long n = Long.parseLong(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        sb.append(n*n).append("\n").append(2);
+        System.out.println(sb);
     }
 }
