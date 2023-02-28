2bc88c312bd647662c2e9ca69be0b3a03910cdc1
minjo-on <alswns11346@gmail.com>
2023-02-28 20:40:30 +0900

    Baekjoon.24266

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 public class Main {//왜 틀린거임
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        System.out.println(n*(n-1)/2+"\n"+2);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(n*n*n+"\n"+3);
     }
 }
