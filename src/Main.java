96fa28af0651797c7bb4cacecdd66f3ab713e8d3
minjo-on <alswns11346@gmail.com>
2023-03-21 19:11:04 +0900

    Baekjoon.13241

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,24 @@ import java.util.Scanner;
 public class Main {//왜 틀린거임
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i=0;i<n;i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(lcm(a,b));
-        }
-
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        System.out.println(lcm(a,b));
     }
-    static int gbc(int a,int b){
+    static long gbc(long a,long b){
         if(a<b){
-            int temp = a;
+            long temp = a;
             a = b;
             b = temp;
         }
         while (b!=0){
-            int r = a%b;
+            long r = a%b;
             a=b;
             b=r;
         }
         return a;
     }
-    static int lcm(int a,int b){
+    static long lcm(long a,long b){
         return a*b / gbc(a,b);
     }
 }
