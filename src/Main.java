6e7829f4656356b11b79ec5af03a07629636b99b
minjo-on <alswns11346@gmail.com>
2023-03-21 19:04:26 +0900

    Baekjoon.1934

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,27 @@ public class Main {//왜 틀린거임
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.print(n*n*n+"\n"+3);
+        for(int i=0;i<n;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(lcm(a,b));
+        }
+
+    }
+    static int gbc(int a,int b){
+        if(a<b){
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        while (b!=0){
+            int r = a%b;
+            a=b;
+            b=r;
+        }
+        return a;
+    }
+    static int lcm(int a,int b){
+        return a*b / gbc(a,b);
     }
 }
