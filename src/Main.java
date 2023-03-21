9678237e2520e39ee5042a932fb88e6f1cd44f53
minjo-on <alswns11346@gmail.com>
2023-03-21 20:11:07 +0900

    Baekjoon.2355

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,14 @@
 import java.util.Scanner;
-public class Main {//왜 틀린거임
+public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
-        int num1 = a*d + b*c;
-        int num2 = b*d;
+        long n = sc.nextLong();
+        long j = sc.nextLong();
+        long a = Math.max(n,j);
+        long b = Math.min(n,j);
+        long l1 = b*(b-1)/2;
+        long l2 = a*(a+1)/2;
 
-        while(true){
-            a = gbc(num1,num2);
-            if(a==1){
-                System.out.print(num1+" "+num2);
-                break;
-            }else{
-                num1 /= a;
-                num2 /= a;
-            }
-        }
-
-    }
-    static int gbc(int a,int b){
-        if(a<b){
-            int temp = a;
-            a = b;
-            b = temp;
-        }
-        while (b!=0){
-            int r = a%b;
-            a=b;
-            b=r;
-        }
-        return a;
+        System.out.println(l2-l1);
     }
 }
