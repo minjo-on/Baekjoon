b346d64d540ff3da63e057df1f6d66df3381612c
minjo-on <103498687+minjo-on@users.noreply.github.com>
2023-03-29 22:21:13 +0900

    4134

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,17 @@
 import java.util.Scanner;
+import java.math.BigInteger;
 public class Main {
-    static int[] arr;
-    static boolean[] visited;
-    static int n, m;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-
-        arr = new int[m];
-        visited = new boolean[n];
-
-        dfs(0);
-    }
-
-    static void dfs(int depth) {
-        if (depth == m) {
-            for (int val : arr) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-            return;
+        StringBuffer sb = new StringBuffer();
+        int t_case = sc.nextInt();
+        for(int i=0;i<t_case;i++){
+            long n = sc.nextLong();
+            BigInteger b = BigInteger.valueOf(n);
+            if(b.isProbablePrime(10)){
+                sb.append(n+"\n");
+            }else sb.append(b.nextProbablePrime()+"\n");
         }
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arr[depth] = i + 1;
-                dfs(depth + 1);
-                visited[i] = false;
-            }
-        }
+        System.out.println(sb);
     }
 }
