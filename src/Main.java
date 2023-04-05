9e76d7d4bdf36a3ba34437bf60926664443e9ebb
minjo-on <103498687+minjo-on@users.noreply.github.com>
2023-04-05 20:50:50 +0900

    1085

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int w = sc.nextInt();
+        int h = sc.nextInt();
 
-        System.out.println(n*m);
+        int x_min = Math.min(x,w-x);
+        int y_min = Math.min(y,h-y);
+
+        System.out.println(Math.min(x_min,y_min));
     }
 }
