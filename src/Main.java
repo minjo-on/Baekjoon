b38ce4fba5fedc4dbca218c3af7370088e43586b
minjo-on <103498687+minjo-on@users.noreply.github.com>
2023-04-06 14:26:17 +0900

    10101

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int x;
-        int y;
-        int max_x = -10001;
-        int max_y = -10001;
-        int min_x = 10001;
-        int min_y = 10001;
-        for(int i=0;i<n;i++){
-            x = sc.nextInt();
-            y = sc.nextInt();
-            max_x = Math.max(x,max_x);
-            max_y = Math.max(y,max_y);
-            min_x = Math.min(x,min_x);
-            min_y = Math.min(y,min_y);
+        int[] angle = new int[3];
+        int sum = 0;
+        for(int i=0;i<3;i++){
+            angle[i] = sc.nextInt();
+            sum+=angle[i];
         }
-        if(n == 1){
-            System.out.print(0);
-        }else {
-            System.out.print((max_x-min_x)*(max_y-min_y));
+        if(sum!=180){
+            System.out.println("Error");
+        }else{
+            if (angle[0]==angle[1]&&angle[1]==angle[2]){
+                System.out.println("Equilateral");
+            }else if (angle[0]==angle[1]||angle[1]==angle[2]||angle[0]==angle[2]) {
+                System.out.println("Isosceles");
+            }else{
+                System.out.println("Scalene");
+            }
         }
     }
 }
