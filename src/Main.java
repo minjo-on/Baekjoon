dba21488b658ce75c44636e02eab00b0572a93d5
minjo-on <103498687+minjo-on@users.noreply.github.com>
2023-04-06 15:06:54 +0900

    14215

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] length = new int[3];
-        while (true){
-            int max = 0;
-            int sum = 0;
-            for(int i=0;i<3;i++){
-                length[i] = sc.nextInt();
-                sum+=length[i];
-                max = Math.max(max,length[i]);
-            }
-            if(sum==0) break;
-            if(max>=sum-max){
-                System.out.println("Invalid");
-            }else{
-                if (length[0]==length[1]&&length[1]==length[2]) {
-                    System.out.println("Equilateral");
-                }else if (length[0]==length[1]||length[1]==length[2]||length[0]==length[2]) {
-                    System.out.println("Isosceles");
-                }else {
-                    System.out.println("Scalene");
-                }
-            }
+        int max = 0;
+        int sum = 0;
+        for(int i=0;i<3;i++){
+            length[i] = sc.nextInt();
+            sum +=length[i];
+            max = Math.max(max,length[i]);
+        }
+        if(max<sum-max){
+            System.out.println(sum);
+        }else{
+            System.out.println((sum-max)*2-1);
         }
     }
 }
